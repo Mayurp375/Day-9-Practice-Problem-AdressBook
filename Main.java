f27f2633eeccafb2a0cfbc5de7d60dd65f79eb47
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-14 00:00:14 +0530

    all parameters are aded

--- Main.java
+++ Main.java
@@ -3,5 +3,16 @@ package classProblemByAmolMateSir.Day9.addressBook;
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to address Book");
+
+        AdressBook adressBook = new AdressBook();
+        adressBook.setFirstName("mayur");
+        adressBook.lastName = "polojwar";
+        adressBook.address = "cha";
+        adressBook.city = "mo";
+        adressBook.state = "mh";
+        adressBook.adress = new Address();
+        adressBook.adress.setPhoneNumber(78556662l);
+        adressBook.adress.setZip(440045);
+        System.out.println(adressBook);
     }
 }
