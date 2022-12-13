1c79537ce2a3a8f2677a0a798094c5b310e77eb0
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-14 00:07:54 +0530

    using scanner

--- Main.java
+++ Main.java
@@ -1,18 +1,37 @@
 package classProblemByAmolMateSir.Day9.addressBook;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to address Book");
+        Scanner scanner = new Scanner(System.in);
+        AdressBook adressBook = new AdressBook(); //object is crated of class
 
-        AdressBook adressBook = new AdressBook();
-        adressBook.setFirstName("mayur");
-        adressBook.lastName = "polojwar";
-        adressBook.address = "cha";
-        adressBook.city = "mo";
-        adressBook.state = "mh";
-        adressBook.adress = new Address();
-        adressBook.adress.setPhoneNumber(78556662l);
-        adressBook.adress.setZip(440045);
+        System.out.println("enter first name");
+        adressBook.firstName = scanner.next();
+
+        System.out.println("enter last name");
+        adressBook.lastName = scanner.next();
+
+        System.out.println("enter address name");
+        adressBook.address = scanner.next();
+
+        System.out.println("enter city name");
+        adressBook.city = scanner.next();
+
+        System.out.println("enter state name");
+        adressBook.state = scanner.next();
+
+
+        adressBook.adress = new Address(); //class object assined
+
+        System.out.println("enter PhoneNumber name");
+        adressBook.adress.setPhoneNumber(scanner.nextLong());
+
+        System.out.println("enter Zip name");
+        adressBook.adress.setZip(scanner.nextInt());
+
         System.out.println(adressBook);
     }
 }
